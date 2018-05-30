239bc7c4d157908d4ded9725aa9ea0e927611791
Diogo Gomes <dgomes@av.it.pt>
2018-05-30 01:29:28 +0100

    bump

--- weather/ipma.py
+++ weather/ipma.py
@@ -18,7 +18,7 @@ from homeassistant.const import \
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers import config_validation as cv
 
-REQUIREMENTS = ['pyipma==1.0.4']
+REQUIREMENTS = ['pyipma==1.0.5']
 
 _LOGGER = logging.getLogger(__name__)
 
