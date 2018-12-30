a4e416d0ae860805d905753e54f6379cf9d2c380
Diogo Gomes <dgomes@av.it.pt>
2018-12-30 02:04:49 +0000

    cleaner

--- energy_tariff_pt.py
+++ energy_tariff_pt.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import (
     async_track_point_in_utc_time, async_track_utc_time_change)
 from homeassistant.const import (
-    STATE_UNKNOWN)
+    CONF_NAME, STATE_UNKNOWN)
 from homeassistant.util import dt as dt_util
 from homeassistant.helpers.entity_component import EntityComponent
 import homeassistant.helpers.config_validation as cv
@@ -32,12 +32,15 @@ ATTR_TARIFF_PERIOD = 'Periodo Horário'
 
 CONF_TARIFF_PERIOD = 'tariff_period'
 
+DEFAULT_NAME = 'Energy Tariff'
+
 CONF_BI_HORARIO_DIARIO = 'bi-horario-diario'
 CONF_BI_HORARIO_SEMANAL = 'bi-horario-semanal'
 CONF_TRI_HORARIO = 'tri-horario'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Required(CONF_TARIFF_PERIOD): vol.Any(CONF_BI_HORARIO_DIARIO,
                                                   CONF_BI_HORARIO_SEMANAL, 
                                                   CONF_TRI_HORARIO) 
@@ -49,8 +52,9 @@ async def async_setup(hass, config):
     component = EntityComponent(_LOGGER, DOMAIN, hass)
     conf = config[DOMAIN]
 
-    energy_tariff = EnergyTariff(hass, conf[CONF_TARIFF_PERIOD]) 
-    
+    energy_tariff = EnergyTariff(hass, conf[CONF_NAME], conf[CONF_TARIFF_PERIOD]) 
+   
+    # update every 15min
     async_track_utc_time_change(hass, energy_tariff.timer_update, minute=range(0,60,15))
     await component.async_add_entities([energy_tariff])
     return True
@@ -59,23 +63,33 @@ async def async_setup(hass, config):
 class EnergyTariff(Entity):
     """Representation of the current EnergyTariff."""
 
-    def __init__(self, hass, tariff):
+    def __init__(self, hass, name, tariff):
         """Initialize the sun."""
         self.hass = hass
+        self._name = name
         self._tariff = tariff
 
     async def async_added_to_hass(self):
+        """Calculate the current tariff on start."""
         self.timer_update(dt_util.now())
 
     @property
     def name(self):
         """Return the name."""
-        return "Energy Tariff"
+        return self._name 
 
     @property
     def state(self):
         """Return the state of the sun."""
         return STATE_VAZIO_NORMAL
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the sensor."""
+        state_attr = {
+            ATTR_TARIFF_PERIOD: self._tariff
+        }
+        return state_attr
 
     def bi_horario_diario(self, time):
         if 0 <= time.hour < 8:
@@ -88,10 +102,10 @@ class EnergyTariff(Entity):
         if 0 <= time.weekday() < 5:
             if 0 <= time.hour < 8:
                 return STATE_VAZIO_NORMAL
-            
         if time.weekday() == 5: 
             # Hora legal de Verão começa no 1º Domingo de Março e acaba no ultimo de Outubro
-            d = datetime(dt.year, 4, 1)   # ends last Sunday in October
+            # https://docs.python.org/3.3/library/datetime.html
+            d = datetime(dt.year, 4, 1)  
             i_verao = d - timedelta(days=d.weekday() + 1)
             d = datetime(dt.year, 11, 1)
             f_verao = d - timedelta(days=d.weekday() + 1)
@@ -103,13 +117,9 @@ class EnergyTariff(Entity):
                 elif 22 <= time.hour < 24:      
                     return STATE_VAZIO_NORMAL
             else:
-                if 0 <= time.hour < 9:
+                if datetime.time(0,00) <= time.time() < datetime.time(9,30):
                     return STATE_VAZIO_NORMAL
-                if time.hour == 9 and time.minutes < 30:
-                    return STATE_VAZIO_NORMAL
-                elif 13 <= time.hour < 18:      
-                    return STATE_VAZIO_NORMAL
-                if time.hour == 18 and time.minutes < 30:
+                elif datetime.time(13,00) <= time.time() < datetime.time(18,30):
                     return STATE_VAZIO_NORMAL
                 elif 22 <= time.hour < 24:      
                     return STATE_VAZIO_NORMAL
