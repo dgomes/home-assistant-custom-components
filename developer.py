e3da04dfff4f4284820522fbbdc845b408848c85
Diogo Gomes <dgomes@av.it.pt>
2019-01-05 00:19:10 +0000

    fix restore

--- developer.py
+++ developer.py
@@ -7,22 +7,28 @@ developer:
   github_personal_token: 3456784235482398577563485739
 
 get your token from https://github.com/settings/tokens
-
 """
 import logging
 from datetime import timedelta
 
 import voluptuous as vol
 
-from homeassistant.const import ATTR_FRIENDLY_NAME
-from homeassistant.helpers.restore_state import async_get_last_state
-from homeassistant.helpers import event
 import homeassistant.helpers.config_validation as cv
+from homeassistant.core import callback
+from homeassistant.const import (
+    ATTR_ENTITY_ID, ATTR_UNIT_OF_MEASUREMENT, CONF_ICON, CONF_NAME, CONF_MODE,
+    STATE_UNAVAILABLE)
+from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.helpers import event
 import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = ['PyGithub']
+
+DOMAIN = 'developer'
+ENTITY_ID_FORMAT = DOMAIN + '.{}'
 
 HOMEASSITANT_ORGANIZATION = 'home-assistant'
 HOMEASSITANT_REPO = 'home-assistant'
@@ -30,8 +36,6 @@ HOMEASSITANT_REPO = 'home-assistant'
 ATTR_LAST_UPDATED = 'last_updated'
 
 NOTIFICATION_ID = 'developer_notification'
-
-DOMAIN = 'developer'
 
 FRIENDLY_NAME = 'Last reviewed PR'
 ENTITY_ID = 'developer.last_signaled_pr'
@@ -42,30 +46,59 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: {
     vol.Optional(CONF_GITHUB_PERSONAL_TOKEN, default=None): cv.string,
 }}, extra=vol.ALLOW_EXTRA)
 
+
 async def async_setup(hass, config):
-    """Set up the hardware owner and developer component."""
-    conf = config.get(DOMAIN, {})
-    github_personal_token = conf[CONF_GITHUB_PERSONAL_TOKEN]
+    """Set up an input slider."""
+    component = EntityComponent(_LOGGER, DOMAIN, hass)
+    await component.async_add_entities(
+        [HADeveloperEntity(config[DOMAIN].get(CONF_GITHUB_PERSONAL_TOKEN))])
+    return True
 
-    last_state = await async_get_last_state(hass, ENTITY_ID)
-    _LOGGER.debug("Last state: %s", last_state)
-    if last_state:
-        hass.states.async_set(ENTITY_ID, last_state.state,
-                              {ATTR_FRIENDLY_NAME: FRIENDLY_NAME})
 
-    def check_new_pullrequests(now):
+class HADeveloperEntity(RestoreEntity):
+    """Representation of a Home Assistant Developer."""
+
+    def __init__(self, github_personal_token):
+        """Initialize an input number."""
+        self.entity_id = ENTITY_ID
+        self._state = STATE_UNAVAILABLE
+        self.github_personal_token = github_personal_token
+
+    @property
+    def should_poll(self):
+        """If entity should be polled."""
+        return False
+
+    @property
+    def name(self):
+        """Return the name of the repository."""
+        return FRIENDLY_NAME 
+
+    @property
+    def state(self):
+        """Return the state of the component."""
+        return self._state
+
+    @callback
+    def check_new_pullrequests(self, now):
         """Check on github for pull requests on platforms currently running."""
         from github import Github
+        from github.GithubException import RateLimitExceededException
         _LOGGER.debug("check_new_pullrequests")
 
-        github_client = Github(github_personal_token)
-        organization = github_client.get_organization(HOMEASSITANT_ORGANIZATION)
-        repository = organization.get_repo(HOMEASSITANT_REPO)
+        print(self.github_personal_token)
+        github_client = Github(self.github_personal_token)
+        try:
+            organization = github_client.get_organization(HOMEASSITANT_ORGANIZATION)
+            repository = organization.get_repo(HOMEASSITANT_REPO)
+        except RateLimitExceededException as err:
+            _LOGGER.error(err)
+            return
 
-        last_signaled_pr = hass.states.get(ENTITY_ID)
+        last_signaled_pr = self._state 
         _LOGGER.debug("Last signaled PR: %s", last_signaled_pr)
 
-        installed_platforms = [p.split('.')[1] for p in list(hass.config.components) if "." in p]
+        installed_platforms = [p.split('.')[1] for p in list(self.hass.config.components) if "." in p]
         installed_platforms = set([p for p in installed_platforms if p not in ['homeassistant']])
         _LOGGER.debug(installed_platforms)
 
@@ -74,8 +107,8 @@ async def async_setup(hass, config):
             found = False
             _LOGGER.debug(pull_request.number)
             pr_list.append(pull_request.number)
-            if last_signaled_pr and\
-                pull_request.number <= int(last_signaled_pr.state):
+            if last_signaled_pr != STATE_UNAVAILABLE and\
+                pull_request.number <= int(last_signaled_pr):
                 break
 
             for changed_file in pull_request.get_files():
@@ -86,7 +119,7 @@ async def async_setup(hass, config):
                         platform in changed_file.filename.split('/')[-1]:
                         _LOGGER.debug("FOUND an interesting PR %s",
                                       pull_request.number)
-                        hass.components.persistent_notification.create(
+                        self.hass.components.persistent_notification.create(
                             '&#35;{}: {}<br />'
                             '{}<br />'
                             'You are using {}'
@@ -97,14 +130,17 @@ async def async_setup(hass, config):
                                                           pull_request.number))
                         found = True
 
-        _LOGGER.debug("Set %s to %s", ENTITY_ID, max(pr_list))
-        hass.states.set(ENTITY_ID, max(pr_list),
-                        {ATTR_FRIENDLY_NAME: FRIENDLY_NAME})
+        _LOGGER.debug("Set %s to %s", self.name, max(pr_list))
+        self._state = max(pr_list)
 
-    # Update daily, start 1 hour after startup
-    _dt = dt_util.utcnow() + timedelta(hours=1)
-    event.async_track_utc_time_change(
-        hass, check_new_pullrequests,
-        hour=_dt.hour, minute=_dt.minute, second=_dt.second)
+    async def async_added_to_hass(self):
+        """Run when entity about to be added to hass."""
+        await super().async_added_to_hass()
+        state = await self.async_get_last_state()
+        if state:
+            self._state = state.state
 
-    return True 
+        _dt = dt_util.utcnow() + timedelta(seconds=10)
+        event.async_track_utc_time_change(
+            self.hass, self.check_new_pullrequests,
+            hour=_dt.hour, minute=_dt.minute, second=_dt.second)
